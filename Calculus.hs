7f522d31d43497010ea54a62344d3dfd1d309e89
Duku, Tyrell <tyrell.duku19@imperial.ac.uk>
2019-10-29 16:20:03 +0000

    CalculusDone.hs

--- Calculus.hs
+++ Calculus.hs
@@ -4,10 +4,10 @@ import Data.Maybe
 
 data UnOp = Neg | Sin | Cos | Log
           deriving (Eq, Ord, Show)
-
+-- UNARY OPERATOR -- takes ONE argument
 data BinOp = Add | Mul | Div
            deriving (Eq, Ord, Show)
-
+-- BINARY OPERATOR -- takes TWO arguments
 data Exp = Val Double | Id String | UnApp UnOp Exp | BinApp BinOp Exp Exp
          deriving (Eq, Ord, Show)
 
@@ -66,26 +66,64 @@ instance Floating Exp where
   atanh   = undefined
 
 ---------------------------------------------------------------------------
-
 lookUp :: Eq a => a -> [(a, b)] -> b
-lookUp
-  = undefined
-
+lookUp keySought envir = fromJust (lookup keySought envir)
+----------------------------------------------------------------------------
 showExp :: Exp -> String
-showExp
-  = undefined
+showExp ( Val v ) = show v 
+showExp ( Id str ) = str
+showExp ( UnApp op exp) = "(" ++ lookUp op dictionaryUn ++ showExp exp ++ ")"
+  where
+    dictionaryUn = [(Neg, "-"), (Sin, "sin") , (Cos, "cos"), (Log, "log")]
+showExp (BinApp bi x1 x2) = "(" ++ showExp x1 ++ lookUp bi dictionaryBin ++ showExp x2 ++ ")"
+  where
+    dictionaryBin = [(Add, "+" ), (Mul, "*" ), (Div, "/" )]
 
+------------------------------------------------------------------------------
 eval :: Exp -> Env -> Double
-eval
-  = undefined
+eval (Val v) _ = v
+eval (Id search) envir = lookUp search envir
+eval (UnApp op ex) envir = lookUp op dictUn (eval ex envir)
+    where
+      dictUn = [( Neg, ((-1) * )) , (Sin, sin ) , (Cos, cos ), (Log, log) ]
+eval (BinApp bi x1 x2) envir = lookUp bi dictBin (eval x1 envir) (eval x2 envir)
+  where
+    dictBin = [(Add, (+)), (Mul, (*)), (Div, (/))]
+
+--------------------------------------------------------------
 
 diff :: Exp -> String -> Exp
-diff
-  = undefined
-
+diff (Val v) _  = Val 0 -- rules for differentiating constants
+diff (Id x) wrt 
+    | x == wrt  = Val 1
+    | otherwise = Val 0
+--rules for differentiating expressions when not in terms of the 
+--correct constant; treats it as if it were a constant.    
+diff (UnApp op expr) wrt
+    | op == Neg   = UnApp Neg (diff expr wrt)
+    | op == Sin   =  BinApp Mul (UnApp Cos expr) (diff expr wrt)
+    | op == Cos   = UnApp Neg (BinApp Mul (UnApp Sin expr) (diff expr wrt))
+    | op == Log   = BinApp Div (diff expr wrt) (expr) 
+diff (BinApp bi x1 x2) wrt
+    | bi == Add = BinApp Add (diff x1 wrt)  (diff x2 wrt)
+    | bi == Mul = BinApp Add product2 product1
+    | otherwise = BinApp Div (quotient1) (quotient2)
+      where
+        quotient1 = BinApp Add (BinApp Mul x1 (diff x2 wrt)) (UnApp Neg (BinApp Mul  (diff x1 wrt ) x2 ))
+        quotient2 = BinApp Mul x2 x2
+        product1 = BinApp Mul (diff x1 wrt) (x2)
+        product2 = BinApp Mul (x1) (diff x2 wrt)
+-----------------------------------------------------------------
 maclaurin :: Exp -> Double -> Int -> Double
-maclaurin
-  = undefined
+maclaurin expr x n 
+  = sum ( zipWith3 (\a b c -> (a*b) / c ) evaluated powersOfX factorials)
+    where
+      factorials     = scanl (*) 1 [1..]
+-- scanl recursively applies function to compute factorials of numbers; 
+-- no need to take n values.
+      functionValues = take n (iterate( flip diff "x") expr)
+      powersOfX      = iterate  (x * ) 1
+      evaluated      = map (flip eval  [("x", 0)]) functionValues
 
 ---------------------------------------------------------------------------
 -- Test cases...
